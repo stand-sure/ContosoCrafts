342851d6d10eb959010ecb32edf0793436ec19de
cecilphillip <cecilphillip@gmail.com>
2020-07-10 14:32:19 -0400

    add order processor

---
+++ src/ContosoCrafts.CheckoutProcessor/Controllers/DaprController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.CheckoutProcessor.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DaprController : ControllerBase
+    {
+        private readonly ILogger<DaprController> logger;
+        public DaprController(ILogger<DaprController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet("subscribe")]
+        public ActionResult Subscribe()
+        {
+            var payload = new[]
+            {
+                new {topic= "checkout", route = "checkout" }
+            };
+            return Ok(payload);
+        }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CheckoutController : ControllerBase
+    {
+        private readonly ILogger<CheckoutController> logger;
+        public CheckoutController(ILogger<CheckoutController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpPost]
+        public ActionResult CheckoutOrder()
+        {
+            logger.LogInformation("Order received...");
+            return Ok();
+        }
+    }
+}
