4c48cc85456f32b69604efe8af9fb1f1a43e7b05
cecilphillip <cecilphillip@gmail.com>
2020-07-21 00:37:08 -0400

    simplify controller

--- src/ContosoCrafts.CheckoutProcessor/Controllers/DaprController.cs
+++ src/ContosoCrafts.CheckoutProcessor/Controllers/DaprController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.CheckoutProcessor.Controllers
@@ -11,6 +7,12 @@ namespace ContosoCrafts.CheckoutProcessor.Controllers
     [Route("[controller]")]
     public class DaprController : ControllerBase
     {
+        private readonly ILogger<DaprController> logger;
+        public DaprController(ILogger<DaprController> logger)
+        {
+            this.logger = logger;
+        }
+
         [HttpGet("subscribe")]
         public ActionResult Subscribe()
         {
@@ -20,19 +22,8 @@ namespace ContosoCrafts.CheckoutProcessor.Controllers
             };
             return Ok(payload);
         }
-    }
 
-    [ApiController]
-    [Route("[controller]")]
-    public class CheckoutController : ControllerBase
-    {
-        private readonly ILogger<CheckoutController> logger;
-        public CheckoutController(ILogger<CheckoutController> logger)
-        {
-            this.logger = logger;
-        }
-
-        [HttpPost]
+        [HttpPost("/checkout")]
         public ActionResult CheckoutOrder()
         {
             logger.LogInformation("Order received...");
