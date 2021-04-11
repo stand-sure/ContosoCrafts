ee26bf36d4086491ef69a3a9b8a77c6a278117c7
cecilphillip <cecilphillip@gmail.com>
2021-04-11 10:03:27 -0400

    add backend host

---
+++ src/ContosoCrafts.Web.Server/Services/IProductService.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ContosoCrafts.Web.Shared.Models;
+
+namespace ContosoCrafts.Web.Server.Services
+{
+    public interface IProductService
+    {
+        Task AddRating(string productId, int rating);
+        Task<IEnumerable<Product>> GetProducts();
+    }
+}
